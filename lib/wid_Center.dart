c3f5aada9a68e2003cca745891b90d0f7aa15edd
Krishnakant491 <krishnakant.97200100@gmail.com>
2025-07-24 21:47:12 +0530

    Center and Conatiner Widget

---
+++ lib/wid_Center.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Center Widget',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Center Widget')),
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 100,
+          color: Colors.amber,
+          child: Text("This is widget center > container > text "),
+        ),
+      ),
+    );
+  }
+}
