90c4e9d3fbb2e96c72de413a11c67d454cd1e043
Krishnakant491 <krishnakant.97200100@gmail.com>
2025-07-26 02:15:55 +0530

    Row Widget

--- lib/Widget/wid_Row.dart
+++ lib/Widget/wid_Row.dart
@@ -38,11 +38,17 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Row(
         mainAxisAlignment: MainAxisAlignment.center,
+        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,   //will give equal space between widgets and border to start and end
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,  //First item at start, last at end, space between others
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,   //will give equal space between widgets but provide half space to start and end
+        // mainAxisAlignment: MainAxisAlignment.end,           //widget will placed at the end
         children: <Text>[
           Text("A - ", style: TextStyle(color: Colors.deepPurple)),
           Text("B - ", style: TextStyle(color: Colors.deepPurple)),
           Text("C - ", style: TextStyle(color: Colors.deepPurple)),
           Text("D - ", style: TextStyle(color: Colors.deepPurple)),
+
+          // Container( ... )   //will give an error because we declare Text widget to Row widget
         ],
       ),
     );
